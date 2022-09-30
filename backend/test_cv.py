bfdb38b6515e150337275ee61fad68c6b0c9f995
Anuar <anuar31415@gmail.com>
2022-09-30 08:39:33 +0600

    added argument parser

--- backend/test_cv.py
+++ backend/test_cv.py
@@ -1,3 +1,14 @@
+import argparse
 from utils.detect.detect import get_detection_result
 
-get_detection_result('facenet','/mnt/harddrive/projects/face_swapper_/test.mp4', debug=True)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--model_type', required=True, help='mediapipe or facenet')
+parser.add_argument('--filepath', required=True, help='path to videofile')
+args = parser.parse_args()
+
+model_type = args.model_type
+filepath = args.filepath
+
+get_detection_result(model_type, filepath, debug=True)
