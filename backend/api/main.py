11cf0869b859079b90f2c0f1b8c81a406321d26f
Kirill Marchenko <kiril0108@gmail.com>
2022-10-02 22:03:40 +0600

    update upload endpoint

--- backend/api/main.py
+++ backend/api/main.py
@@ -7,7 +7,7 @@ from os import path
 import werkzeug
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-from utils.detect import get_detection_result
+from utils.detect.detect import Detectors, get_detection_result
 
 UPLOAD_FOLDER = os.path.join(path.dirname(path.abspath(__file__)), 'uploads')
 if not os.path.exists(UPLOAD_FOLDER):
@@ -20,18 +20,25 @@ api = Api(app)
 class upload(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
+    parser.add_argument('detector_type', type=Detectors.argparse, location='form')
 
     def post(self):
         request_data = self.parser.parse_args()
-        print(f'\nINFO: Data from request:\n\t{request_data}\n')
+        print(f'\nINFO: Data from request:')
+        for key, value in request_data.items():
+            print(f"\t{key}: {value}")
 
+        # handle file argument
         file = request_data['file']
         filename = file.filename
         filepath = os.path.join(UPLOAD_FOLDER, filename)
         file.save(filepath)
-        print(f'INFO: {filename} saved to {UPLOAD_FOLDER}\n')
+        print(f'\nINFO: {filename} saved to {UPLOAD_FOLDER}\n')
 
-        detection_result = get_detection_result(filepath)
+        # handle optional detect model type
+        detector_type = request_data['detector_type']
+
+        detection_result = get_detection_result(detector_type, filepath)
         response = app.response_class(
             response=detection_result,
             status=200,
