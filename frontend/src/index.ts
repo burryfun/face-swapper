a80bf88414199ad0e2de6a584655bf0b5fdcd12e
Kirill Marchenko <kiril0108@gmail.com>
2022-10-03 21:33:27 +0600

    remove trash

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,39 +1,14 @@
 import './style.css'
 import { VideoHandler } from './videoHandler';
 
-
+// TODO: Вынести в отдельный namespace api
 const API_URL = 'http://localhost:5000'
-
-let video = document.getElementById('input-video') as HTMLMediaElement;
-let videoSource = document.createElement('source') as HTMLSourceElement;
 
 const fileInput = document.getElementById('file-input');
 
 const videoHandler = new VideoHandler(document.querySelector('.content-container'));
 
 fileInput?.addEventListener('change', async (event) => {
-  // if (video.hasChildNodes()) {
-  //   // TODO: Выделить в отдельную функцию/класс инстанцирование видео
-  //   video.remove();
-  //   video.removeChild(videoSource);
-  //   let videoContainer = document.querySelector('.video-container');
-  //   const newVideo = document.createElement('video');
-
-  //   newVideo.setAttribute('controls', 'true');
-  //   newVideo.id = 'input-video';
-  //   newVideo.width = 320;
-    
-  //   videoContainer?.appendChild(newVideo);
-  //   video = newVideo;
-  // }
-  // event.preventDefault();
-
-
-  // video.addEventListener('timeupdate', () => {
-  //   if (video.currentTime > 2) {
-  //     console.log(video.currentTime);
-  //   }
-  // })
 
   const formData = new FormData();
   let file = (event.target as HTMLInputElement).files![0];  
@@ -41,14 +16,11 @@ fileInput?.addEventListener('change', async (event) => {
   if (file) {
     formData.append('file', file);
     
-    // const src = URL.createObjectURL(file);
-    // videoSource.setAttribute('src', src)
-    // video?.appendChild(videoSource);
-    
     videoHandler.loadFile(file);
   }
 
   if (formData) {
+    // TODO: Вынести в отдельный namespace api
     try {
       const response = await fetch(`${API_URL}/upload`, {method:'POST', body:formData});
       if (response.ok) {
