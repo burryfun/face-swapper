d0b9882b9369023e9f949da75334863c8a8ee906
Kirill Marchenko <kiril0108@gmail.com>
2022-10-04 23:37:12 +0600

    add api launch on localhost:5000

--- backend/docker_run.sh
+++ backend/docker_run.sh
@@ -1,2 +1,2 @@
 xhost +local:docker
-docker run -it --rm --gpus all --network host --name faceswap_cv -v $(pwd)/test_videos:/usr/src/app/test_videos -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY faceswap_cv
+docker run -it -p 5000:5000 --rm --gpus all --name faceswap_cv -v $(pwd)/test_videos:/usr/src/app/test_videos -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY faceswap_cv
